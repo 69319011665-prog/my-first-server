c10e7e47b8b294f17c616bb66f58b2536d3a2462
69319011665-prog <69319011665@techno-chon.ac.th>
2026-07-23 12:27:21 +0700

    sever.js

--- sever.js
+++ sever.js
@@ -11,12 +11,12 @@ res.statusCode = 200;
 res.setHeader('Content-Type', 'text/html; charset=utf-8');
 
 try {
-// 3. ขอเชื่อมตอและสงคําสั่ง SQL ไปดึงข้อมูลจากตาราง students
+// 3. ขอเชื่อมตอและสงคําสั่ง SQL ไปดึงขอมูลจากตาราง students
 const client = await pool.connect();
 const result = await client.query('SELECT * FROM students');
-client.release(); // คนืการเชื่อมต่อเมื่อใช้งานเสร็จ
-// 4. นําข้อมูลที่ได้(result.rows) มาประกอบเป็นตาราง HTML
-let html = `<h1>ฐานข้อมูลนักศึกษา (ทดสอบการเชื่อมต่อ)</h1>`;
+client.release(); // คนืการเชื่อมตอเมื่อใชงานเสร็จ
+// 4. นําขอมูลที่ได(result.rows) มาประกอบเปนตาราง HTML
+let html = `<h1>ฐานขอมูลนักศึกษา (ทดสอบการเชื่อมตอ)</h1>`;
 html += `<table border="1" cellpadding="10">`;
 html += `<tr><th>69319011665</th><th>นนท์ธิดา วังโส</th></tr>`;
 // วนลูปนําขอมูลแตละแถวมาแสดง
@@ -26,7 +26,7 @@ html += `<tr><td>${row.student_id}</td><td>${row.student_name}</td></tr>`;
 html += `</table>`;
 res.end(html);
 } catch (err) {
-// กรณเีชื่อมต่อไม่ได้หรือเขียนชื่อตารางผิด
+// กรณเีชื่อมตอไมไดหรือเขียนชื่อตารางผิด
 console.error(err);
 res.end(`<h1>เกิดขอผิดพลาด!</h1><p>${err.message}</p>`);
 }
